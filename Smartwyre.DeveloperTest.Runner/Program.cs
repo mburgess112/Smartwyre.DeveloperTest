64548381032df682bac815002b8e7d3c2f55e527
Matt Burgess <mburgess112@hotmail.co.uk>
2024-12-02 10:31:12 +0000

    Include basic test-runner setup

--- Smartwyre.DeveloperTest.Runner/Program.cs
+++ Smartwyre.DeveloperTest.Runner/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Smartwyre.DeveloperTest.Data;
+using Smartwyre.DeveloperTest.Services;
+using Smartwyre.DeveloperTest.Types;
+
+using System;
 
 namespace Smartwyre.DeveloperTest.Runner;
 
@@ -6,6 +10,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        throw new NotImplementedException();
+        // need to validate/document args: can use CommandLineRunner to specify
+        var request = new CalculateRebateRequest
+        {
+            ProductIdentifier = args[0],
+            RebateIdentifier = args[1],
+            Volume = decimal.Parse(args[2])
+        };
+        Console.WriteLine($"Processing request: {request}");
+
+        // should ideally have some base logic in the real data stores for integration testing
+        var service = new RebateService(new ProductDataStore(), new RebateDataStore());
+
+        var result = service.Calculate(request);
+        Console.WriteLine($"Calculated successfully: {result.Success}");
     }
 }
